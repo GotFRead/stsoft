e5b614d20f3bfdfc7e3ebbd0cbbd0ac26f18ca12
TrishinA <trishin.aoff@mail.ru>
2024-06-15 18:28:31 +0300

    feat: Добавил касскадное удаление таймлайнов

--- src/timeline/models.py
+++ src/timeline/models.py
@@ -1,4 +1,5 @@
 from models.base import Base
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from datetime import datetime
@@ -6,7 +7,11 @@ from datetime import datetime
 
 class TimeIntervals(Base):
     __tablename__ = "time_intervals"
-    task_id: Mapped[int]
-    owner_id: Mapped[int]
+    task_id: Mapped[int] = mapped_column(
+        ForeignKey("tasks.id", ondelete="CASCADE")
+    )
+    owner_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE")
+    )
     time_start: Mapped[datetime] 
     time_end: Mapped[datetime]
